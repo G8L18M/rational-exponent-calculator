96e153ffaef87409d7f44381a3948227bf75e31c
G8L18M <garelnow@gmail.com>
2025-02-08 13:46:07 -0800

    Update RationalExponentCalculatorDecimalClass.py

--- RationalExponentCalculatorDecimalClass.py
+++ RationalExponentCalculatorDecimalClass.py
@@ -6,7 +6,7 @@ N, R = list(map(Decimal, input().split()))
 n = int(input())
 decimalPlaces = int(input())
 
-getcontext().prec = decimalPlaces
+getcontext().prec = decimalPlaces + 10
 
 isNegative = False
 
@@ -16,6 +16,10 @@ if R == 0:
 
 if N / R < 0:
     isNegative = True
+    N = abs(N)
+    R = abs(R)
+
+if N < 0 and R < 0:
     N = abs(N)
     R = abs(R)
 
@@ -29,10 +33,13 @@ elif (N / R) == int(N / R):
     print(T ** (N // R))
 else:
     D = math.ceil(math.log(2, abs(T)))
-    L_X = 2 ** (D * math.floor((D - 1) / R))
-    L_Y = 2 ** (math.floor((D - 1) / R))
-    U_X = 2 ** (R * math.ceil(D / R))
-    U_Y = 2 ** (math.ceil(D / R))
+    L_X = Decimal(2.0) ** (D * math.floor((D - 1) / R))
+    L_Y = Decimal(2.0) ** (math.floor((D - 1) / R))
+    U_X = Decimal(2.0) ** (R * math.ceil(D / R))
+    U_Y = Decimal(2.0) ** (math.ceil(D / R))
+
+    #print(str(L_X) + "\n\n" + str(L_Y) + "\n\n" + str(U_X) + "\n\n" + str(U_Y))
+
     A = (T / abs(T)) * (L_Y + ((abs(T) - L_X) / (U_X - L_X)) * (U_Y - L_Y))
     for i in range(n):
         A_new = A - (2 * (A ** (R + 1)) - 2 * T * A) / (R * (A ** R) + A ** R + T * R - T)
